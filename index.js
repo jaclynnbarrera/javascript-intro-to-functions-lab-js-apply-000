364576548efa3550d106794b479a5121fedf4874
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-10-02 02:54:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase ( )
 }
-function logShout(string) {
-  return console.log ("HELLO")
+function shout(string) {
+  return console.log ()
 }
